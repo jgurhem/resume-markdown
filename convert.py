2bbcabcd3bc9ef1c809a3cd88193001eed683d14
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2019-12-16 23:36:08 +0100

    Add option to manage double column id in HTML div tag

--- convert.py
+++ convert.py
@@ -1,17 +1,30 @@
 from weasyprint import HTML, CSS
 import commonmark
 import sys, os
+import argparse
 
-if len(sys.argv) != 2:
-  print("Usage : python3 " + sys.argv[0] + " resume.md")
-  sys.exit(1)
+parser = argparse.ArgumentParser(description="Convert Markdown file to pdf")
+parser.add_argument("input", help="Markdown file to convert in pdf.")
+parser.add_argument("-o", "--output", dest="output", help="Output file.", type=str, default=None)
+parser.add_argument("--double-column", dest="dc", help="Use double columns.", action="store_true")
+args = parser.parse_args()
 
-
-with open(sys.argv[1]) as fp:
+with open(args.input) as fp:
   text = fp.read()
   html = commonmark.commonmark(text)
   html = HTML(string=html, encoding='utf-8')
   css = CSS(string='@page { size: A4; margin: 1cm }')
   css_file = CSS('./style.css')
-  out_file = os.path.splitext(sys.argv[1])[0] + '.pdf'
-  html.write_pdf(out_file, stylesheets=[css, css_file])
+  css_file_dc = CSS('./style_doublecolumn.css')
+
+  if args.output == None:
+    out_file = os.path.splitext(args.input)[0] + '.pdf'
+  else:
+    out_file = args.output
+
+  if args.dc:
+    stylesheets = [css, css_file, css_file_dc]
+  else:
+    stylesheets = [css, css_file]
+
+  html.write_pdf(out_file, stylesheets=stylesheets)
